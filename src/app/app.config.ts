d83633207c6f6f75dcb60a3b6dfb05eada1f8dcd
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2024-11-27 14:09:15 +0100

    add firebases and tools for angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"notes-8da1a","appId":"1:131685614663:web:d353ebfc34a2a5b3f31d62","storageBucket":"notes-8da1a.firebasestorage.app","apiKey":"AIzaSyALSmDNsCgq_cx5F8s2chMHl_WTAt60Q9g","authDomain":"notes-8da1a.firebaseapp.com","messagingSenderId":"131685614663"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
